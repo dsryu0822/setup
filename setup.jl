611ccfd566fddbcad3a8bd1805075dfd367e67d5
DaeSick <47377298+rmsmsgood@users.noreply.github.com>
2021-11-08 08:33:04 +0900

    Update setup.jl

--- setup.jl
+++ setup.jl
@@ -3,10 +3,10 @@ using Pkg
 lib_list =
  ["NearestNeighbors", "DataFrames", "Graphs",
  "Profile", "Plots", "Statistics", "Random",
- "Distributions", "CSV", "StatsBase"]
+ "Distributions", "CSV", "StatsBase", "LaTeXStrings"]
 
 @time Pkg.add(string.(lib_list))
 
 @time using NearestNeighbors, DataFrames, Graphs
 @time using Profile, Plots, Statistics, Random
-@time using Distributions, CSV, StatsBase
+@time using Distributions, CSV, StatsBase, LaTeXStrings
