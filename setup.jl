3f49a9655b141b055d423668306080b099efda10
DaeSick <47377298+rmsmsgood@users.noreply.github.com>
2021-12-22 10:50:30 +0900

    XLSX 추가

--- setup.jl
+++ setup.jl
@@ -3,12 +3,12 @@ using Pkg
 lib_list =
  ["NearestNeighbors", "DataFrames", "Graphs",
  "Profile", "Plots", "Statistics", "Random",
- "Distributions", "CSV", "StatsBase", "LaTeXStrings",
+ "Distributions", "XLSX", "CSV", "StatsBase", "LaTeXStrings",
  "StatsPlots", "Pluto"]
 
 @time Pkg.add(string.(lib_list))
 
 @time using NearestNeighbors, DataFrames, Graphs
 @time using Profile, Plots, Statistics, Random
-@time using Distributions, CSV, StatsBase, LaTeXStrings
+@time using Distributions, XLSX, CSV, StatsBase, LaTeXStrings
 @time using StatsPlots, Pluto
