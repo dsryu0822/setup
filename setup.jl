d591a17c7d59c648d40570692776920622dbcb2c
DaeSick <47377298+rmsmsgood@users.noreply.github.com>
2023-01-02 12:14:50 +0900

    CUDA

--- setup.jl
+++ setup.jl
@@ -6,18 +6,12 @@ lib_list =
  "Distributions", "StatsBase", "LaTeXStrings", "GLM",
  "StatsPlots", "Pluto",
  "JSON", "CSV", "XLSX", "JLD2",
- "Crayons", "SMTPClient"
+ "Crayons", "SMTPClient",
+ "CUDA"
 ]
 
 @time Pkg.add(string.(lib_list))
 @time Pkg.instantiate()
  
-# @time using Distances, NearestNeighbors, BoundingSphere, DataFrames, Graphs
-# @time using Profile, Plots, Statistics, Random
-# @time using Distributions, StatsBase, LaTeXStrings, GLM
-# @time using StatsPlots, Pluto
-# @time using JSON, CSV, XLSX, JLD2
-# @time using Crayons, SMTPClient
-
 using Pluto
 Pluto.run()
