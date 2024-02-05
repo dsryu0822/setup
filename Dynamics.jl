616d0f6c1ae70adad9f422a69cd181dc1c7675f0
dsryu0822 <rmsmsgood@naver.com>
2024-02-05 20:53:39 +0900

    Utils update

---
+++ Dynamics.jl
@@ -0,0 +1,31 @@
+using FiniteDifferences
+using LinearAlgebra
+
+∇(f, x) = first(grad(central_fdm(2, 1), f, x))
+function GD!(f, x; h = 0.001)
+    x .-= h * ∇(f, x)
+    return x
+end
+function findfixed(f, x0; maxitr = 10_000, atol = 1e-6, h = 0.001)
+    x = deepcopy(x0)
+    L(x) = norm(f(x))
+    L_ = Float64[]
+    for t in 1:maxitr
+        GD!(L, x; h = h)
+        push!(L_, L(x))
+        if last(L_) < atol
+            @info """$t / $maxitr iterations
+            $(last(L_)) < atol = $atol"""
+            return x
+        end
+    end
+    @warn """not converged...
+    $(last(L_)) < atol = $atol"""
+    return x
+end
+
+pairwiseadd(X::Vector{T}, Y::Vector{Vector{T}}) where T <: Number = [X + y for y in Y]
+pairwiseadd(X::Vector{Vector{T}}, Y::Vector{Vector{T}}) where T <: Number = reduce(vcat, [pairwiseadd(x, Y) for x in X])
+pairwiseadd(X::Vector{Vector{T}}, Y::Vector{T}) where T <: Number = pairwiseadd(Y, X)
+
+@info "∇, GD!, findfixed, pairwiseadd are defined"
--- Portables.jl
+++ Portables.jl
@@ -1,2 +1,44 @@
 head(x) = eval(Meta.parse("$(x)[$(":, " ^ (length(size(x))-1)) 1:(end-1)]"))
 tail(x) = eval(Meta.parse("$(x)[$(":, " ^ (length(size(x))-1)) 2: end   ]"))
+
+function argext(arr)
+    idxmin = (circshift(arr, 1) .> arr) .&& (circshift(arr, -1) .> arr)
+    idxmax = (circshift(arr, 1) .< arr) .&& (circshift(arr, -1) .< arr)
+    return idxmin .|| idxmax
+end
+
+# x = [missing, missing, 3, 4, 7, missing, missing, 9, missing, 0, missing]
+
+function padmissing(x)
+    if x .|> !ismissing |> all
+        return x
+    elseif x .|> ismissing |> all
+        @error "padmissing: x is all missing"
+    end
+    x = deepcopy(Vector{Union{Float64, Missing}}(x))
+    if first(x) |> ismissing
+        k = findfirst(!ismissing, x)
+        x[1:k-1] .= x[k]
+    end
+    if last(x) |> ismissing
+        k = findlast(!ismissing, x)
+        x[k+1:end] .= x[k]
+    end
+    y = diff(ismissing.(x))
+    heads = findall(y .== 1)
+    tails = findall(y .== -1) .+ 1
+
+    argitpl = UnitRange.(heads, tails)
+    valitpl = LinRange.(x[heads], x[tails], tails - heads .+ 1)
+    x[vcat(argitpl...)] .= vcat(valitpl...)
+
+    return x
+end
+# [x padmissing(x)]
+
+⊕(x, y) = [x; y]
+⊕(x, y...) = reduce(⊕, [x, y...])
+
+@info "head, tail, argext, padmissing are defined"
+@info "operations ⊕ are defined"
+@info "Int64 is overloaded to String"
